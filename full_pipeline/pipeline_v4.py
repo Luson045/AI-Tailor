e5c3774bf03bf7349c6a2fa034a574e081717b60
Luson10 <luson.basumatary@nimroboai.com>
2025-11-11 19:15:56 +0530

    added interactive landing page

--- full_pipeline/pipeline_v4.py
+++ full_pipeline/pipeline_v4.py
@@ -13,9 +13,9 @@ from mpl_toolkits.mplot3d import Axes3D
 # ========== CONFIG ==========
 # Use front + left + right views for best results
 IMAGES = {
-    'front': "dataset/image13.jpg",
-    'left': "dataset/image13_right.jpg",   # Left side view (person's left visible)
-    'right': "dataset/image13_left.jpg", # Right side view (person's right visible)
+    'front': "dataset/image6.jpg",
+    'left': "dataset/image6_right.jpg",   # Left side view (person's left visible)
+    'right': "dataset/image6_left.jpg", # Right side view (person's right visible)
 }
 MODEL_PATH = "full_pipeline/models/"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
